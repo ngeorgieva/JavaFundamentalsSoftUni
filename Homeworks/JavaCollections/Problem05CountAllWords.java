62312e664dce6b625a50ec1827de36912d876122
ngeorgieva <nat.m.georgieva@gmail.com>
2016-03-29 14:37:21 +0300

    Regex corrected

--- Homeworks/JavaCollections/Problem05CountAllWords.java
+++ Homeworks/JavaCollections/Problem05CountAllWords.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Problem05CountAllWords {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String[] input = scan.nextLine().split("\\s*\\W\\s*");
+        String[] input = scan.nextLine().split("\\s*[^A-Za-z]\\s*");
         System.out.println(input.length);
     }
 }
