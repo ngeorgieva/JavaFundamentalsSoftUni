8af2da155842624023c2b5075302d86d7bab4343
ngeorgieva <nat.m.georgieva@gmail.com>
2016-04-02 16:33:00 +0300

    Small data type changes and optimization

--- Homeworks/JavaCollections/Problem08ExtractAllUniqueWords.java
+++ Homeworks/JavaCollections/Problem08ExtractAllUniqueWords.java
@@ -4,14 +4,13 @@ public class Problem08ExtractAllUniqueWords {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String[] input = scan.nextLine().split("\\s*\\W\\s*");
-        ArrayList<String> words = new ArrayList<>();
+        TreeSet<String> words = new TreeSet<>();
         for (String s : input) {
             if (!words.contains(s.toLowerCase())){
                 words.add(s.toLowerCase());
             }
         }
 
-        Collections.sort(words);
         for (String word : words) {
             System.out.print(word + " ");
         }
